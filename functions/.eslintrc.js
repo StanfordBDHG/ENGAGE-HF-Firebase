8e4ce61cf4b22e6e1cdfea9f1140a073cb704847
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-06-18 10:24:51 -0700

    Use NextJSTemplate as much as possible/reasonable

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -7,17 +7,97 @@
 //
 
 module.exports = {
-  root: true,
-  env: {
-    es2020: true,
-    node: true,
+  plugins: ['prettier', 'eslint-plugin-import', '@typescript-eslint', 'import'],
+  parserOptions: {
+    project: './tsconfig.json',
   },
+  ignorePatterns: ['**/*.js', '**/*.jsx'],
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "google",
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "max-len": ["error", {"code": 150}],
+    'prettier/prettier': 'error',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling']],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-empty-named-blocks': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-cycle': 'error',
+    'import/extensions': [
+      'warn',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+      },
+    ],
+    'import/newline-after-import': 'warn',
+    'import/no-anonymous-default-export': 'warn',
+    'import/no-default-export': 'error',
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+        disallowTypeAnnotations: false,
+      },
+    ],
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    'import/no-duplicates': [
+      'error',
+      {
+        'prefer-inline': true,
+      },
+    ],
+    // false negatives
+    'import/namespace': ['off'],
+    'no-empty-pattern': 'off',
+    // make sure to `await` inside try…catch
+    '@typescript-eslint/return-await': ['error', 'in-try-catch'],
+    '@typescript-eslint/no-confusing-void-expression': [
+      'error',
+      { ignoreArrowShorthand: true },
+    ],
+    // empty interfaces are fine, e.g. React component that extends other component, but with no additional props
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/array-type': [
+      'warn',
+      { default: 'array-simple', readonly: 'array-simple' },
+    ],
+    // allow unused vars prefixed with `_`
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    // numbers and booleans are fine in template strings
+    '@typescript-eslint/restrict-template-expressions': [
+      'error',
+      { allowNumber: true, allowBoolean: true },
+    ],
   },
-};
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,100 +9,141 @@
 // Based on:
 // https://github.com/StanfordBDHG/PediatricAppleWatchStudy/pull/54/files
 
-import { CallableRequest, onCall } from "firebase-functions/v2/https";
-import { logger, https } from "firebase-functions/v2";
-import { FieldValue } from "firebase-admin/firestore";
-import admin = require("firebase-admin");
-import { AuthBlockingEvent, beforeUserCreated } from "firebase-functions/v2/identity";
+import * as admin from 'firebase-admin'
+import { FieldValue, type Transaction } from 'firebase-admin/firestore'
+import { type BlockingFunction } from 'firebase-functions'
+import { logger, https } from 'firebase-functions/v2'
+import { type CallableRequest, onCall } from 'firebase-functions/v2/https'
+import {
+  type AuthBlockingEvent,
+  beforeUserCreated,
+} from 'firebase-functions/v2/identity'
 
-admin.initializeApp();
+admin.initializeApp()
 
-type InvitationCodeInput = {
-    invitationCode: string
+interface InvitationCodeInput {
+  invitationCode: string
 }
 
-exports.checkInvitationCode = onCall(async (request: CallableRequest<InvitationCodeInput>) => {
-  if (!request.auth?.uid) {
-    throw new https.HttpsError(
-        "unauthenticated",
-        "User is not properly authenticated.",
-    );
-  }
-
-  const userId = request.auth.uid;
-  const {invitationCode} = request.data;
-
-  const firestore = admin.firestore();
-  logger.debug(`User (${userId}) -> ENGAGE-HF, InvitationCode ${invitationCode}`);
-
-  try {
-    // Based on https://github.com/StanfordSpezi/SpeziStudyApplication/blob/main/functions/index.js
-    const invitationCodeRef = firestore.doc(`invitationCodes/${invitationCode}`);
-    const invitationCodeDoc = await invitationCodeRef.get();
-
-    if (!invitationCodeDoc.exists || invitationCodeDoc.data()?.used) {
-      throw new https.HttpsError("not-found", "Invitation code not found or already used.");
+export const checkInvitationCode = onCall(
+  async (request: CallableRequest<InvitationCodeInput>) => {
+    if (!request.auth?.uid) {
+      throw new https.HttpsError(
+        'unauthenticated',
+        'User is not properly authenticated.',
+      )
     }
 
-    const userStudyRef = firestore.doc(`users/${userId}`);
-    const userStudyDoc = await userStudyRef.get();
+    const userId = request.auth.uid
+    const { invitationCode } = request.data
 
-    if (userStudyDoc.exists) {
-      throw new https.HttpsError("already-exists", "User is already enrolled in the study.");
+    const firestore = admin.firestore()
+    logger.debug(
+      `User (${userId}) -> ENGAGE-HF, InvitationCode ${invitationCode}`,
+    )
+
+    try {
+      // Based on https://github.com/StanfordSpezi/SpeziStudyApplication/blob/main/functions/index.js
+      const invitationCodeRef = firestore.doc(
+        `invitationCodes/${invitationCode}`,
+      )
+      const invitationCodeDoc = await invitationCodeRef.get()
+
+      if (!invitationCodeDoc.exists || invitationCodeDoc.data()?.used) {
+        throw new https.HttpsError(
+          'not-found',
+          'Invitation code not found or already used.',
+        )
+      }
+
+      const userStudyRef = firestore.doc(`users/${userId}`)
+      const userStudyDoc = await userStudyRef.get()
+
+      if (userStudyDoc.exists) {
+        throw new https.HttpsError(
+          'already-exists',
+          'User is already enrolled in the study.',
+        )
+      }
+
+      // eslint-disable-next-line @typescript-eslint/require-await
+      await firestore.runTransaction(async (transaction: Transaction) => {
+        transaction.set(userStudyRef, {
+          invitationCode: invitationCode,
+          dateOfEnrollment: FieldValue.serverTimestamp(),
+        })
+
+        transaction.update(invitationCodeRef, {
+          used: true,
+          usedBy: userId,
+        })
+      })
+
+      logger.debug(
+        `User (${userId}) successfully enrolled in study (ENGAGE-HF) with invitation code: ${invitationCode}`,
+      )
+
+      return {}
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error(`Error processing request: ${error.message}`)
+        if ('code' in error) {
+          throw new https.HttpsError('internal', 'Internal server error.')
+        }
+      } else {
+        logger.error(`Unknown error: ${String(error)}`)
+      }
+      throw error
     }
+  },
+)
 
-    await firestore.runTransaction(async (transaction: any) => {
-      transaction.set(userStudyRef, {
-        invitationCode: invitationCode,
-        dateOfEnrollment: FieldValue.serverTimestamp(),
-      });
+export const beforecreated: BlockingFunction = beforeUserCreated(
+  async (event: AuthBlockingEvent) => {
+    const firestore = admin.firestore()
+    const userId = event.data.uid
 
-      transaction.update(invitationCodeRef, {
-        used: true,
-        usedBy: userId,
-      });
-    });
-
-    logger.debug(`User (${userId}) successfully enrolled in study (ENGAGE-HF) with invitation code: ${invitationCode}`);
-
-    return {};
-  } catch (error: any) {
-    logger.error(`Error processing request: ${error.message}`);
-    if (!error.code) {
-      throw new https.HttpsError("internal", "Internal server error.");
-    }
-    throw error;
-  }
-});
-
-exports.beforecreated = beforeUserCreated(async (event: AuthBlockingEvent) => {
-  const firestore = admin.firestore();
-  const userId = event.data.uid;
-
-  try {
-    // Check Firestore to confirm whether an invitation code has been associated with a user.
-    const invitationQuerySnapshot = await firestore.collection("invitationCodes")
-        .where("usedBy", "==", userId)
+    try {
+      // Check Firestore to confirm whether an invitation code has been associated with a user.
+      const invitationQuerySnapshot = await firestore
+        .collection('invitationCodes')
+        .where('usedBy', '==', userId)
         .limit(1)
-        .get();
+        .get()
 
-    logger.info(`Invitation code query snapshot: ${invitationQuerySnapshot.size}`);
+      logger.info(
+        `Invitation code query snapshot: ${invitationQuerySnapshot.size}`,
+      )
 
-    if (invitationQuerySnapshot.empty) {
-      throw new https.HttpsError("not-found", `No valid invitation code found for user ${userId}.`);
+      if (invitationQuerySnapshot.empty) {
+        throw new https.HttpsError(
+          'not-found',
+          `No valid invitation code found for user ${userId}.`,
+        )
+      }
+
+      const userDoc = await firestore.doc(`users/${userId}`).get()
+
+      // Check if the user document exists and contains the correct invitation code.
+      if (
+        !userDoc.exists ||
+        userDoc.data()?.invitationCode !== invitationQuerySnapshot.docs[0].id
+      ) {
+        throw new https.HttpsError(
+          'failed-precondition',
+          'User document does not exist or contains incorrect invitation code.',
+        )
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error(`Error processing request: ${error.message}`)
+        if ('code' in error) {
+          throw new https.HttpsError('internal', 'Internal server error.')
+        }
+      } else {
+        logger.error(`Unknown error: ${String(error)}`)
+      }
+      throw error
     }
-
-    const userDoc = await firestore.doc(`users/${userId}`).get();
-
-    // Check if the user document exists and contains the correct invitation code.
-    if (!userDoc.exists || userDoc.data()?.invitationCode !== invitationQuerySnapshot.docs[0].id) {
-      throw new https.HttpsError("failed-precondition", "User document does not exist or contains incorrect invitation code.");
-    }
-  } catch (error: any) {
-    logger.error(`Error processing request: ${error.message}`);
-    if (!error.code) {
-      throw new https.HttpsError("internal", "Internal server error.");
-    }
-    throw error;
-  }
-});
+  },
+)
