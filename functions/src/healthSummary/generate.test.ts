33a98074044ded3f1bc926e36e761bd0e5793f42
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-07-03 12:30:28 -0700

    Update tests to use lfs files

--- functions/src/healthSummary/generate.test.ts
+++ functions/src/healthSummary/generate.test.ts
@@ -1,26 +1,56 @@
-import { assert } from 'chai'
+import fs from 'fs'
+import { assert, expect } from 'chai'
 import { generateHealthSummary } from './generate.js'
 import { mockHealthSummaryData } from '../tests/mocks/healthSummaryData.js'
 
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+
 describe('generateHealthSummary', () => {
+  const exportActualPdfs = false
+  const pdfMetadataSuffixLength = 1000
+
   it('should still create as nice of a PDF as before', async () => {
-    const data = mockHealthSummaryData()
-    const result = await generateHealthSummary(data)
-    assert.equal(result.length, 23581335)
+    const inputData = mockHealthSummaryData()
+    const actualData = await generateHealthSummary(inputData)
+    if (exportActualPdfs)
+      fs.writeFileSync(
+        'src/tests/resources/mockHealthSummary-actual.pdf',
+        actualData,
+      )
+    const expectedData = fs.readFileSync(
+      'src/tests/resources/mockHealthSummary.pdf',
+    )
+    assert.equal(actualData.length, expectedData.length)
+    const maxIndex = actualData.length - pdfMetadataSuffixLength
+    expect(
+      Buffer.from(actualData).compare(expectedData, 0, maxIndex, 0, maxIndex),
+    ).to.equal(0)
   })
 
   it('should not fail on empty data', async () => {
-    const data = mockHealthSummaryData()
-    data.dateOfBirth = undefined
-    data.nextAppointment = undefined
-    data.medications = []
-    data.symptomScores = []
-    data.vitals.systolicBloodPressure = []
-    data.vitals.diastolicBloodPressure = []
-    data.vitals.heartRate = []
-    data.vitals.bodyWeight = []
-    data.vitals.dryWeight = NaN
-    const result = await generateHealthSummary(data)
-    assert.equal(result.length, 8587295)
+    const inputData = mockHealthSummaryData()
+    inputData.dateOfBirth = undefined
+    inputData.nextAppointment = undefined
+    inputData.medications = []
+    inputData.symptomScores = []
+    inputData.vitals.systolicBloodPressure = []
+    inputData.vitals.diastolicBloodPressure = []
+    inputData.vitals.heartRate = []
+    inputData.vitals.bodyWeight = [{ date: new Date('2000-01-01'), value: NaN }]
+    inputData.vitals.dryWeight = NaN
+    const actualData = await generateHealthSummary(inputData)
+    if (exportActualPdfs)
+      fs.writeFileSync(
+        'src/tests/resources/emptyHealthSummary-actual.pdf',
+        actualData,
+      )
+    const expectedData = fs.readFileSync(
+      'src/tests/resources/emptyHealthSummary.pdf',
+    )
+    assert.equal(actualData.length, expectedData.length)
+    const maxIndex = actualData.length - pdfMetadataSuffixLength
+    expect(
+      Buffer.from(actualData).compare(expectedData, 0, maxIndex, 0, maxIndex),
+    ).to.equal(0)
   })
 })
--- functions/src/healthSummary/generateSpeedometer.test.ts
+++ functions/src/healthSummary/generateSpeedometer.test.ts
@@ -1,23 +1,39 @@
 import assert from 'assert'
+import fs from 'fs'
 import { describe, it } from 'mocha'
 import { generateSpeedometerSvg } from './generateSpeedometer.js'
 import { mockHealthSummaryData } from '../tests/mocks/healthSummaryData.js'
 
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+
 describe('generateSpeedometer', () => {
+  const exportActualSvgs = false
+
   it('should generate the same chart on mock data', () => {
-    const data = mockHealthSummaryData()
-    const svg = generateSpeedometerSvg(data.symptomScores, 258)
-    assert.equal(
-      svg,
-      '<svg width="258" height="138.67999999999998" xmlns="http://www.w3.org/2000/svg"><g><defs><linearGradient id="gradient"><stop offset="0%" stop-color="rgb(255,0,31)"></stop><stop offset="50%" stop-color="rgb(255,191,47)"></stop><stop offset="100%" stop-color="rgb(0,127,63)"></stop></linearGradient></defs><text x="129" y="71.77999999999999" style="text-anchor: middle; font-size: 36pt; font-weight: bold; fill: rgb(0,121,251);">75<tspan style="font-size: 18pt;">  %</tspan></text><text x="129" y="101.77999999999999" style="text-anchor: middle; font-size: 10pt;"><tspan style="fill: rgb(0,127,63);">▲+31%</tspan><tspan> from previous</tspan></text><path transform="translate(129, 105.77999999999999)" style="fill: url(#gradient);" d="M-103.2,0A103.2,103.2,0,1,1,103.2,0L90.3,0A90.3,90.3,0,1,0,-90.3,0Z"></path><path d="M 116.1 105.77999999999999 L 141.9 105.77999999999999" stroke-dasharray="3.685714285714286,3.685714285714286" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none" transform="translate(-29.897394205776152,-92.01471795155611) rotate(-108,129,105.77999999999999)"></path><path d="M 116.1 105.77999999999999 L 141.9 105.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(0,121,251)" fill="none" transform="translate(68.41258107979847,-68.41258107979846) rotate(-45,129,105.77999999999999)"></path><path d="M 116.1 105.77999999999999 L 141.9 105.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none" transform="translate(-18.12914218616883,-95.03629150800062) rotate(-100.8,129,105.77999999999999)"></path><text x="42.7" y="101.77999999999999" style="text-anchor: left; font-size: 8pt; font-weight: bold; stroke: rgb(145,145,145);">0%</text><text x="48.83571428571429" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Baseline</text><path d="M 25.8 121.77999999999999 L 45.150000000000006 121.77999999999999" stroke-dasharray="3.685714285714286,3.685714285714286" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none"></path><text x="117.63571428571427" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Previous</text><path d="M 94.6 121.77999999999999 L 113.94999999999999 121.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none"></path><text x="186.43571428571428" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Current</text><path d="M 163.4 121.77999999999999 L 182.75 121.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(0,121,251)" fill="none"></path></g></svg>',
+    const inputData = mockHealthSummaryData()
+    const actualData = generateSpeedometerSvg(inputData.symptomScores, 258)
+    if (exportActualSvgs)
+      fs.writeFileSync(
+        'src/tests/resources/mockSpeedometer-actual.svg',
+        actualData,
+      )
+    const expectedData = fs.readFileSync(
+      'src/tests/resources/mockSpeedometer.svg',
     )
+
+    assert.equal(actualData, expectedData.toString('utf8'))
   })
 
   it('should generate an empty chart the same way', () => {
-    const svg = generateSpeedometerSvg([], 258)
-    assert.equal(
-      svg,
-      '<svg width="258" height="138.67999999999998" xmlns="http://www.w3.org/2000/svg"><g><defs><linearGradient id="gradient"><stop offset="0%" stop-color="rgb(255,0,31)"></stop><stop offset="50%" stop-color="rgb(255,191,47)"></stop><stop offset="100%" stop-color="rgb(0,127,63)"></stop></linearGradient></defs><path transform="translate(129, 105.77999999999999)" style="fill: url(#gradient);" d="M-103.2,0A103.2,103.2,0,1,1,103.2,0L90.3,0A90.3,90.3,0,1,0,-90.3,0Z"></path><text x="42.7" y="101.77999999999999" style="text-anchor: left; font-size: 8pt; font-weight: bold; stroke: rgb(145,145,145);">0%</text><text x="48.83571428571429" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Baseline</text><path d="M 25.8 121.77999999999999 L 45.150000000000006 121.77999999999999" stroke-dasharray="3.685714285714286,3.685714285714286" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none"></path><text x="117.63571428571427" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Previous</text><path d="M 94.6 121.77999999999999 L 113.94999999999999 121.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(145,145,145)" fill="none"></path><text x="186.43571428571428" y="125.77999999999999" style="text-anchor: left; font-size: 8pt;">Current</text><path d="M 163.4 121.77999999999999 L 182.75 121.77999999999999" stroke-dasharray="none" stroke-width="3.685714285714286" stroke="rgb(0,121,251)" fill="none"></path></g></svg>',
+    const actualData = generateSpeedometerSvg([], 258)
+    if (exportActualSvgs)
+      fs.writeFileSync(
+        'src/tests/resources/emptySpeedometer-actual.svg',
+        actualData,
+      )
+    const expectedData = fs.readFileSync(
+      'src/tests/resources/emptySpeedometer.svg',
     )
+    assert.equal(actualData, expectedData.toString('utf8'))
   })
 })
