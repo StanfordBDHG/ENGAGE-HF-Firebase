462fb2a608f9dc3cb071ea45421839d3169bc50e
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-07-08 15:28:07 -0700

    Make rebuildStaticData a function with heavy reuse but different impl for prod and debug

--- functions/src/functions/staticData.ts
+++ functions/src/functions/staticData.ts
@@ -1,15 +1,37 @@
 import admin from 'firebase-admin'
-import { onRequest } from 'firebase-functions/v2/https'
+import { CallableRequest, onCall, onRequest } from 'firebase-functions/v2/https'
 import { RxNormService } from '../services/rxNormService.js'
 import { StaticDataService } from '../services/staticDataService.js'
 
-export const rebuildStaticDataFunction = onRequest(async (_, res) => {
+const isEmulator = process.env.FUNCTIONS_EMULATOR === 'true'
+
+async function rebuildStaticData(userId: string | undefined) {
+  if (!isEmulator) {
+    if (!userId)
+      throw new Error('User is not properly authenticated')
+  
+    const user = await admin.auth().getUser(userId)
+    if (!user.customClaims?.admin)
+      throw new Error('User is not an admin')
+  }
   const service = new StaticDataService(
     './data/',
     admin.firestore(),
     new RxNormService(),
   )
   await service.updateAll()
-  res.write('Success', 'utf8')
-  res.end()
+}
+
+const rebuildStaticDataFunctionProduction = onCall(async (request: CallableRequest<void>) => {
+  await rebuildStaticData(request.auth?.uid)
+  return 'Success'
 })
+
+
+const rebuildStaticDataFunctionDebug = onRequest(async (_, response) => {
+  await rebuildStaticData(undefined)
+  response.write('Success', 'utf8')
+  response.end()
+})
+
+export const rebuildStaticDataFunction = isEmulator ? rebuildStaticDataFunctionDebug : rebuildStaticDataFunctionProduction
