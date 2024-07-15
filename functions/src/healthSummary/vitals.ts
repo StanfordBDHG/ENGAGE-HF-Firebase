4c74b1a2cf63b3aa767a5730f8f8941b8cfefa0e
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-07-15 16:10:47 -0700

    Fix conflicts

---
+++ functions/src/healthSummary/vitals.ts
@@ -0,0 +1,20 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+export interface Vitals {
+  systolicBloodPressure: Observation[]
+  diastolicBloodPressure: Observation[]
+  heartRate: Observation[]
+  bodyWeight: Observation[]
+  dryWeight?: Observation
+}
+
+export interface Observation {
+  date: Date
+  value: number
+}
