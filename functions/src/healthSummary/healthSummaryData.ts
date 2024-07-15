4a76921a6717b667b7d8cda32581467e0faad65e
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-07-15 16:10:47 -0700

    Fix conflicts

---
+++ functions/src/healthSummary/healthSummaryData.ts
@@ -0,0 +1,21 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import { type MedicationOptimization } from './medication.js'
+import { type Vitals } from './vitals.js'
+import { type SymptomScores } from '../models/symptomScores.js'
+
+export interface HealthSummaryData {
+  name: string
+  dateOfBirth?: Date
+  clinicianName: string
+  nextAppointment?: Date
+  medications: MedicationOptimization[]
+  vitals: Vitals
+  symptomScores: SymptomScores[]
+}
