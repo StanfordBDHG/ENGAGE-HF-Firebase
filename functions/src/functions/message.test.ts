31a9a7c0c2f059671ec041e2a250995440e04bde
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-07-15 15:49:50 -0700

    Add license header

--- functions/src/functions/message.test.ts
+++ functions/src/functions/message.test.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { assert, expect } from 'chai'
 import admin from 'firebase-admin'
 import { https } from 'firebase-functions/v2'
--- functions/src/functions/message.ts
+++ functions/src/functions/message.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
 import { https } from 'firebase-functions'
 import { type CallableRequest, onCall } from 'firebase-functions/v2/https'
 import { FirestoreService } from '../services/database/firestoreService'
