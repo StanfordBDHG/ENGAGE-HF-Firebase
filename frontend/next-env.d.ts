cce45f62c6b43c971c8cf0e947bf1fb03602ee78
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-08-12 18:30:45 -0700

    reuse

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
