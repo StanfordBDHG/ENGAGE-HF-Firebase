a18d74729c37c0da0f32bc9230fa1f3abd701332
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-08-19 12:26:03 -0700

    Make sure to only change recommendations when needed

--- functions/src/services/patient/databasePatientService.ts
+++ functions/src/services/patient/databasePatientService.ts
@@ -140,7 +140,7 @@ export class DatabasePatientService implements PatientService {
 
           if (newRecommendationIndex < 0) {
             transaction.delete(existing.ref)
-            return
+            continue
           }
           const newRecommendation = newRecommendations[newRecommendationIndex]
           indicesToBeInserted.delete(newRecommendationIndex)
