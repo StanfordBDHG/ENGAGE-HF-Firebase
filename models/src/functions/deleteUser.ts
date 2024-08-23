863beabfb12c7994af0d1369a2045f5229a3aaf8
Paul <pauljohanneskraft@users.noreply.github.com>
2024-08-23 07:01:37 -0700

    readd functions (#98)

---
+++ models/src/functions/deleteUser.ts
@@ -0,0 +1,14 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import { z } from 'zod'
+
+export const deleteUserInputSchema = z.object({
+  userId: z.string(),
+})
+export type DeleteUserInput = z.input<typeof deleteUserInputSchema>
