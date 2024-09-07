b15e81b26dc375f843c424acb7fedfd0e49a4be5
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-09-06 23:08:04 -0700

    post-merge fix

--- functions/src/functions/setupUser.ts
+++ functions/src/functions/setupUser.ts
@@ -11,29 +11,33 @@ import { z } from 'zod'
 import { validatedOnCall } from './helpers.js'
 import { getServiceFactory } from '../services/factory/getServiceFactory.js'
 
-export const setupUser = validatedOnCall(z.unknown(), async (request) => {
-  const userId = request.auth?.uid
-  const invitationCode: unknown = request.auth?.token.invitationCode
-  if (
-    userId === undefined ||
-    invitationCode === undefined ||
-    typeof invitationCode !== 'string'
-  )
-    throw new https.HttpsError('unauthenticated', 'User is not authenticated')
+export const setupUser = validatedOnCall(
+  'setupUser',
+  z.unknown(),
+  async (request) => {
+    const userId = request.auth?.uid
+    const invitationCode: unknown = request.auth?.token.invitationCode
+    if (
+      userId === undefined ||
+      invitationCode === undefined ||
+      typeof invitationCode !== 'string'
+    )
+      throw new https.HttpsError('unauthenticated', 'User is not authenticated')
 
-  const factory = getServiceFactory()
-  const userService = factory.user()
-  const triggerService = factory.trigger()
+    const factory = getServiceFactory()
+    const userService = factory.user()
+    const triggerService = factory.trigger()
 
-  const invitation = await userService.getInvitationByCode(invitationCode)
-  if (invitation === undefined)
-    throw new https.HttpsError('not-found', 'Invitation not found')
+    const invitation = await userService.getInvitationByCode(invitationCode)
+    if (invitation === undefined)
+      throw new https.HttpsError('not-found', 'Invitation not found')
 
-  await userService.enrollUser(invitation, userId)
+    await userService.enrollUser(invitation, userId)
 
-  logger.debug(
-    `User (${userId}) successfully enrolled in the study with invitation code: ${invitationCode}`,
-  )
+    logger.debug(
+      `User (${userId}) successfully enrolled in the study with invitation code: ${invitationCode}`,
+    )
 
-  await triggerService.userEnrolled(userId)
-})
+    await triggerService.userEnrolled(userId)
+  },
+)
