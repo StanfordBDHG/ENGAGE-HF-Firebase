dc67e5188e1058a21d90bb09c7907305fdd1dfc1
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2024-09-10 08:51:17 +0200

    Separate encoded output type

--- functions/models/src/helpers/schemaConverter.ts
+++ functions/models/src/helpers/schemaConverter.ts
@@ -9,11 +9,11 @@
 import { type z } from 'zod'
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-export class SchemaConverter<Schema extends z.ZodType<any, any, any>> {
+export class SchemaConverter<Schema extends z.ZodType<any, any, any>, Encoded> {
   // Properties
 
   readonly schema: Schema
-  readonly encode: (value: z.output<Schema>) => z.input<Schema>
+  readonly encode: (value: z.output<Schema>) => Encoded
 
   get value(): this {
     return this
@@ -23,7 +23,7 @@ export class SchemaConverter<Schema extends z.ZodType<any, any, any>> {
 
   constructor(input: {
     schema: Schema
-    encode: (value: z.output<Schema>) => z.input<Schema>
+    encode: (value: z.output<Schema>) => Encoded
   }) {
     this.schema = input.schema
     this.encode = input.encode
