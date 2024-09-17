62bfc64f440bf11e30c5acbe09f03e7bb885bfe0
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2024-09-17 16:09:56 -0700

    Add service account setup

--- functions/src/functions/helpers.ts
+++ functions/src/functions/helpers.ts
@@ -18,53 +18,21 @@ import {
 } from 'firebase-functions/v2/https'
 import { z } from 'zod'
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 
 export function validatedOnCall<
   Schema extends z.ZodTypeAny,
-  Return = any | Promise<any>,
+  Return = unknown | Promise<unknown>,
 >(
   name: string,
   schema: Schema,
   handler: (request: CallableRequest<z.output<Schema>>) => Return,
+  options: CallableOptions = {
+    invoker: 'public',
+    serviceAccount: `cloudfunctionsserviceaccount@${process.env.GCLOUD_PROJECT}.iam.gserviceaccount.com`,
+  },
 ): CallableFunction<
   z.input<Schema>,
-  Return extends Promise<unknown> ? Return : Promise<Return>
-> {
-  return onCall((request) => {
-    try {
-      logger.debug(
-        `onCall(${name}) from user '${request.auth?.uid}' with ${JSON.stringify(request.data)}`,
-      )
-      request.data = schema.parse(request.data) as z.output<Schema>
-      return handler(request)
-    } catch (error) {
-      logger.debug(
-        `onCall(${name}) from user '${request.auth?.uid}' failed with ${String(error)}.`,
-      )
-      if (error instanceof z.ZodError) {
-        throw new https.HttpsError(
-          'invalid-argument',
-          'Invalid request data',
-          error.errors,
-        )
-      }
-      throw error
-    }
-  })
-}
-
-export function validatedOnCallWithOptions<
-  Schema extends z.ZodTypeAny,
-  Return = any | Promise<any>,
->(
-  name: string,
-  schema: Schema,
-  options: CallableOptions,
-  handler: (request: CallableRequest<z.output<Schema>>) => Return,
-): CallableFunction<
-  z.output<Schema>,
   Return extends Promise<unknown> ? Return : Promise<Return>
 > {
   return onCall(options, (request) => {
@@ -98,37 +66,10 @@ export function validatedOnRequest<Schema extends z.ZodTypeAny>(
     data: z.output<Schema>,
     response: Response,
   ) => void | Promise<void>,
-): https.HttpsFunction {
-  return onRequest(async (request, response) => {
-    try {
-      logger.debug(`onRequest(${name}) with ${JSON.stringify(request.body)}`)
-      const data = schema.parse(request.body) as z.output<Schema>
-      await handler(request, data, response)
-      return
-    } catch (error) {
-      logger.debug(`onRequest(${name}) failed with ${String(error)}.`)
-      if (error instanceof z.ZodError) {
-        response.status(400).send({
-          code: 'invalid-argument',
-          message: 'Invalid request data',
-          details: error.errors,
-        })
-        return
-      }
-      throw error
-    }
-  })
-}
-
-export function validatedOnRequestWithOptions<Schema extends z.ZodTypeAny>(
-  name: string,
-  schema: Schema,
-  options: https.HttpsOptions,
-  handler: (
-    request: Request,
-    data: z.output<Schema>,
-    response: Response,
-  ) => void | Promise<void>,
+  options: https.HttpsOptions = {
+    invoker: 'public',
+    serviceAccount: `cloudfunctionsserviceaccount@${process.env.GCLOUD_PROJECT}.iam.gserviceaccount.com`,
+  },
 ): https.HttpsFunction {
   return onRequest(options, async (request, response) => {
     try {
