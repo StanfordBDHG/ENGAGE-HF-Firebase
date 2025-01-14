af28858c9122c01c30447b8964a5c5fe9a5de0da
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-01-14 18:34:27 +0100

    Add providerName to health summary pdf

--- functions/src/services/healthSummary/databaseHealthSummaryService.ts
+++ functions/src/services/healthSummary/databaseHealthSummaryService.ts
@@ -53,15 +53,16 @@ export class DefaultHealthSummaryService implements HealthSummaryService {
       this.getVitals(userId, advanceDateByDays(new Date(), -14), weightUnit),
     ])
 
-    const clinician =
-      patient?.content.clinician ?
-        await this.userService.getAuth(patient.content.clinician)
-      : undefined
+    const providerName =
+      patient?.content.providerName ??
+      (patient?.content.clinician ?
+        (await this.userService.getAuth(patient.content.clinician)).displayName
+      : undefined)
 
     return {
       name: auth.displayName,
       dateOfBirth: patient?.content.dateOfBirth,
-      providerName: clinician?.displayName,
+      providerName: providerName,
       nextAppointment: nextAppointment?.content,
       recommendations: recommendations.map((doc) => doc.content),
       vitals: vitals,
