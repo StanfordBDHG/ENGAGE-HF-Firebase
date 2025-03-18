ed410a50f6a6f237bd5f10dcaa47af5ecba2f767
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-03-18 12:49:40 +0100

    Update share url to have patients instead of users

--- functions/src/functions/shareHealthSummary.ts
+++ functions/src/functions/shareHealthSummary.ts
@@ -38,7 +38,7 @@ export const shareHealthSummary = validatedOnCall(
     )
 
     const shareCodeDocument = await factory.patient().createShareCode(userId)
-    const url = `${Env.WEB_FRONTEND_BASE_URL}/users/${userId}/healthSummary/${shareCodeDocument.id}`
+    const url = `${Env.WEB_FRONTEND_BASE_URL}/patients/${userId}/healthSummary/${shareCodeDocument.id}`
     return {
       code: shareCodeDocument.content.code,
       expiresAt: dateConverter.encode(shareCodeDocument.content.expiresAt),
